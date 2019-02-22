bda2ac15e10b46907467b5abc74ab46e4267223e
RevanVI <revan0603@gmail.com>
2019-02-22 13:48:02 +0300

    start lab2

---
+++ ooplab1/tcomplex.cpp
@@ -0,0 +1,6 @@
+#include "tcomplex.h"
+
+tcomplex::tcomplex()
+{
+
+}
---
+++ ooplab1/tcomplex.h
@@ -0,0 +1,11 @@
+#ifndef TCOMPLEX_H
+#define TCOMPLEX_H
+
+
+class tcomplex
+{
+public:
+    tcomplex();
+};
+
+#endif // TCOMPLEX_H
