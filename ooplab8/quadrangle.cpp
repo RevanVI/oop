789367db77ea6c5f4e20e8b10a5958db75a69108
RevanVI <revan0603@gmail.com>
2019-03-24 23:10:47 +0300

    base lab8 done

--- ooplab8/quadrangle.cpp
+++ ooplab8/quadrangle.cpp
@@ -5,22 +5,28 @@ double findLen(double x1, double y1, double x2, double y2)
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
-quadrangle::quadrangle(const double val[8])
+quadrangle::quadrangle(const double s[4], const double a[4])
 {
-    for (int i = 0; i < 8; ++i)
-        coords[i] = val[i];
+    for (int i = 0; i < 4; ++i)
+        side[i] = s[i];
+    for (int i = 0; i < 4; ++i)
+        angle[i] = a[i];
 }
 
-quadrangle::quadrangle(double val)
+quadrangle::quadrangle(double s, double a)
 {
-    for (int i = 0; i < 8; ++i)
-        coords[i] = val;
+    for (int i = 0; i < 4; ++i)
+        side[i] = s;
+    for (int i = 0; i < 4; ++i)
+        angle[i] = a;
 }
 
 quadrangle::quadrangle(const quadrangle& obj)
 {
-    for (int i = 0; i < 8; ++i)
-        coords[i] = obj.coords[i];
+    for (int i = 0; i < 4; ++i)
+        side[i] = obj.side[i];
+    for (int i = 0; i < 4; ++i)
+        angle[i] = obj.angle[i];
 }
 
 quadrangle::~quadrangle()
@@ -28,63 +34,128 @@ quadrangle::~quadrangle()
 
 }
 
+double quadrangle::findDiag(int s1, int s2)
+{
+    return sqrt(pow(side[s1], 2) + pow(side[s2], 2) - 2 * side[s1] * side[s2] * cos(angle[s1] * PI / 180));
+}
+
 double quadrangle::area()
 {
+    cout << "Quadrangle area\n";
     if (!isReal())
         return 0.0;
-    //first line
-    double k1 = (coords[1] - coords[5])/(coords[4] - coords[0]);
-    double d1 = findLen(coords[0], coords[1], coords[4], coords[5]);
-    //second line
-    double k2 = (coords[3] - coords[7])/(coords[6] - coords[2]);
-    double d2 = findLen(coords[2], coords[3], coords[6], coords[7]);
+    /*
+    double d1 = findDiag(0, 1);
+    double d2 = findDiag(1, 2);
 
-    double angle = atan((k2 - k1) / (1 + k1*k2));
-    return d1 * d2 * sin(angle * 3.14 / 180) / 2;
+    double a1 = acos((pow(side[1], 2) + d1*d1 - pow(side[0], 2))/(2*pow(side[1], 2)*d1)) / PI * 180;
+    double a2 = acos((pow(side[1], 2) + d2*d2 - pow(side[2], 2))/(2*pow(side[1], 2)*d2)) / PI * 180;
+
+    double a = 180 - a1 - a2;
+    return 0.5 * d1 * d2 * sin(a * PI / 180);
+    */
+    return 0.5 * (side[0] * side[1] * sin(angle[0] * PI / 180) + side[2] * side[3] * sin(angle[2] * PI / 180));
 }
 
 double quadrangle::perimeter()
 {
+    cout << "Quadrangle perimeter\n";
     if (!isReal())
         return 0.0;
     double len = 0.0;
     for(int i = 0; i < 4; ++i)
-        len += sqrt(pow(coords[i * 2] - coords[(i * 2 + 2) % 8], 2) + pow(coords[i * 2 + 1] - coords[(i * 2 + 3) % 8], 2));
+        len += side[i];
     return len;
 }
 
 bool quadrangle::isReal()
 {
-    bool xIsZero = false,
-         yIsZero = false;
+    double sum = 0.0;
     for(int i = 0; i < 4; ++i)
     {
-        double x = coords[i * 2] - coords[(i * 2 + 2) % 8];
-        double y = coords[i * 2 + 1] - coords[(i * 2 + 3) % 8];
-        double len = sqrt(pow(x, 2) + pow(y, 2));
-        if (x < 0.001 || x > -0.001)
-        {
-            if (xIsZero == true)
-                return false;
-            xIsZero = true;
-        }
-        if (y < 0.001 || y > -0.001)
-        {
-            if (yIsZero == true)
-                return false;
-            yIsZero = true;
-        }
-        if (len < 0.001)
+        if ((side[i] < 0))
             return false;
+        sum += angle[i];
     }
+    if (sum > 360.2 || sum < 359.8)
+        return false;
     return true;
 }
 
-parallelogram::parallelogram(double s1, double s2, double angle) : quadrangle (0.0)
+
+
+parallelogram::parallelogram(double s1, double s2, double ang) : quadrangle (s1, ang)
+{
+    side[1] = side[3] = s2;
+    angle[1] = angle[3] = 180 - ang;
+}
+
+parallelogram::~parallelogram()
 {
 
 }
-virtual ~parallelogram() override;
-virtual double area() override;
-virtual double perimeter() override;
-virtual bool isReal() override;
+
+double parallelogram::area()
+{
+    cout << "Parallelogram area\n";
+    if (!isReal())
+        return 0.0;
+    return side[0] * side[1] * sin(angle[0] * PI / 180);
+}
+
+
+
+rhomb::rhomb(double s, double ang): parallelogram (s, s, ang)
+{
+
+}
+
+rhomb::~rhomb()
+{
+
+}
+
+
+rectangle::rectangle(double s1, double s2): parallelogram (s1, s2, 90)
+{
+
+}
+
+rectangle::~rectangle()
+{
+
+}
+
+double rectangle::area()
+{
+    cout << "Rectangle area\n";
+    if(!isReal())
+        return 0.0;
+    return side[0] * side[1];
+}
+
+
+
+square::square(double s): parallelogram (s, s, 90), rectangle (s, s), rhomb (s, 90)
+{
+
+}
+
+square::~square()
+{
+
+}
+
+double square::area()
+{
+    cout << "Square area\n";
+    return rectangle::area();
+}
+
+double square::perimeter()
+{
+    cout << "Square perimeter\n";
+    if (!isReal())
+        return 0.0;
+    return 4 * side[0];
+}
--- ooplab8/quadrangle.h
+++ ooplab8/quadrangle.h
@@ -2,16 +2,24 @@
 #define QUADRANGLE_H
 
 #include <cmath>
+#include <iostream>
+
+using std::cout;
+
+const double PI = 3.14;
 
 double findLen(double x1, double y1, double x2, double y2);
 
 class quadrangle
 {
 protected:
-    double coords[8];
+    double side[4];
+    double angle[4];
+    double findDiag(int s1, int s2);
+
 public:
-    quadrangle(const double[8]);
-    quadrangle(double val = 0.0);
+    quadrangle(const double[4], const double[4]);
+    quadrangle(double s = 0.0, double a = 0.0);
     quadrangle(const quadrangle& obj);
     virtual ~quadrangle();
     virtual double area();
@@ -22,14 +30,10 @@ public:
 class parallelogram: public quadrangle
 {
 protected:
-    double s1, s2;
-    double angle;
 public:
     parallelogram(double s1, double s2, double angle);
     virtual ~parallelogram() override;
     virtual double area() override;
-    virtual double perimeter() override;
-    virtual bool isReal() override;
 };
 
 class rhomb: virtual public parallelogram
@@ -37,22 +41,20 @@ class rhomb: virtual public parallelogram
 public:
     rhomb(double s, double angle);
     virtual ~rhomb() override;
-    virtual double area() override;
-    virtual double perimeter() override;
 };
 
 class rectangle: virtual public parallelogram
 {
 public:
-
+    rectangle(double s1, double s2);
     virtual ~rectangle() override;
     virtual double area() override;
-    virtual double perimeter() override;
 };
 
 class square: public rectangle, public rhomb
 {
 public:
+    square(double s);
     virtual ~square() override;
     virtual double area() override;
     virtual double perimeter() override;
